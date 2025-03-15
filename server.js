3318373755141bc2f4bf52a5feb7f1d67dc7b790
Janikcz13 <janik.cz.123@gmail.com>
2025-03-16 00:01:59 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/submit', (req, res) => {
+    const textData = req.body.text + "\n---\n";
+
+    fs.appendFile(__dirname + '/submissions.txt', textData, (err) => {
+        if (err) {
+            console.error('Error saving submission:', err);
+            return res.status(500).json({ message: 'Error saving submission' });
+        }
+        res.json({ message: 'Submission saved!' });
+    });
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
